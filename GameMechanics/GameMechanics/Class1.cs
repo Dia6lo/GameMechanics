ebd89cd0c77c035ff36e3e5bdbcd68604d5fc21c
aandreev <aandreev@game-forest.com>
2015-12-14 23:31:20 +1000

    Initialize solution

---
+++ GameMechanics/GameMechanics/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameMechanics
+{
+    public class Class1
+    {
+    }
+}
